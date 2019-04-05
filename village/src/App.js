1b9c603d83052bc30e9efaff63a2ca7b7ff9fb9f
tca88 <trishnaamin@gmail.com>
2019-04-05 11:49:54 -0400

    import axios and react-router-dom into App.js and import react-router-dom (withRouter component) into index.js.

--- village/src/App.js
+++ village/src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Route, Link, NavLink } from "react-router-dom";
+import axios from "axios";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
